5279483b954d57f7ae59c9d190c7bf01c0a0a1a2
Roberto Bolgheroni <robertobolgheroni@usp.br>
2024-08-26 15:26:13 -0300

    feat: implmement model_training app draft

--- src/spira_training/apps/model_training/model_training.py
+++ src/spira_training/apps/model_training/model_training.py
@@ -1,8 +1,18 @@
+from src.spira_training.shared.core.services.model_training_service import (
+    ModelTrainingService,
+)
+
+
 def main():
-    # Read env variables
-    # instantiate service
-    # execute service
-    pass
+    # TODO read env variables and instantiate the dependencies
+    service = ModelTrainingService(
+        dataset_repository=None,
+        dataset_splitter=None,
+        model_trainer=None,
+        trained_models_repository=None,
+        config=None,
+    )
+    service.execute()
 
 
 if __name__ == "__main__":
