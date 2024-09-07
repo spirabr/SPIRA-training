16d6826ca31dff6c9fce274831e7c39ae3ea5131
Roberto Bolgheroni <robertobolgheroni@usp.br>
2024-09-07 19:22:28 -0300

    feat: add pyright type checker

--- Makefile
+++ Makefile
@@ -3,3 +3,11 @@ app:
 
 unit-tests: 
 	docker compose run --rm tests
+
+type-check:
+	poetry run pyright
+
+MAIN_DIFF=$(shell git diff main --diff-filter=ACM --name-only | grep ".py") 
+
+type-check-main:	
+	poetry run pyright $(MAIN_DIFF)
--- src/spira_training/apps/model_training/model_training.py
+++ src/spira_training/apps/model_training/model_training.py
@@ -1,12 +1,14 @@
 from src.spira_training.shared.core.services.model_training_service import (
     ModelTrainingService,
 )
+from tests.fakes.fake_dataset_repository import FakeDatasetRepository
 
 
 def main():
     # TODO read env variables and instantiate the dependencies
+    dataset_repository = FakeDatasetRepository()
     service = ModelTrainingService(
-        dataset_repository=None,
+        dataset_repository=dataset_repository,
         dataset_splitter=None,
         model_trainer=None,
         trained_models_repository=None,
