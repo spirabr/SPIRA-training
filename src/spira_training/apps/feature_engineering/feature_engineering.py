a6cc6cd91a50c3e16bd2430db8f9c70ef81866d6
Lucas Quaresma <lucasqml08@usp.br>
2024-09-09 00:01:47 -0300

    create feature_engineering.py

--- src/spira_training/apps/feature_engineering/feature_engineering.py
+++ src/spira_training/apps/feature_engineering/feature_engineering.py
@@ -1,16 +1,14 @@
-from src.spira_training.apps.feature_engineering.app.app import App
+from src.spira_training.shared.core.services.feature_engineering_service import FeatureEngineeringService
 from src.spira_training.shared.core.services.randomizer import Randomizer
 from src.spira_training.shared.ports.config_loader import ConfigLoader
 from src.spira_training.shared.ports.dataset_repository import DatasetRepository
-
-
 def main():
     config = ConfigLoader.load()
     randomizer = Randomizer.initialize_random()
     dataset_repository = DatasetRepository()
+    service = FeatureEngineeringService(config, randomizer, dataset_repository)
 
-    app = App(config, randomizer, dataset_repository)
-    app.execute()
+    service.execute()
 
 if __name__ == "__main__":
     main()
