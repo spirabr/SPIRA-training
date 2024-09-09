730cbbcaef87a257df1d815397a7afa84d994a2f
Lucas Quaresma <lucasqml08@usp.br>
2024-09-09 00:02:10 -0300

    create config for apps

--- src/spira_training/shared/core/models/config.py
+++ src/spira_training/shared/core/models/config.py
@@ -1,2 +1,10 @@
+from src.spira_training.apps.feature_engineering.feature_engineering_config import FeatureEngineeringConfig
+from src.spira_training.apps.model_evaluation.model_evaluation_config import ModelEvaluationConfig
+from src.spira_training.apps.model_publish.model_publish_config import ModelPublishConfig
+
+
 class Config:
-    pass
+    feature_engineering_config: FeatureEngineeringConfig
+    model_trainer_config: ModelTrainerConfig
+    model_evaluation_config: ModelEvaluationConfig
+    model_publish_config: ModelPublishConfig
