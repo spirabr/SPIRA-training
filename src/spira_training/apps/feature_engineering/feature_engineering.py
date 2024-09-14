2411d9a86783cc17bdf59d595a6f0133f0e865eb
Lucas Quaresma <lucasqml08@usp.br>
2024-09-13 22:57:43 -0300

    refactor: inject dependencies for FeatureEngineeringService

--- src/spira_training/apps/feature_engineering/feature_engineering.py
+++ src/spira_training/apps/feature_engineering/feature_engineering.py
@@ -1,13 +1,19 @@
+from src.spira_training.shared.adapters.CSVValidPathReader import CSVValidPathReader
 from src.spira_training.shared.adapters.JsonConfigLoader import JsonConfigLoader
-from src.spira_training.shared.core.services.feature_engineering_service import FeatureEngineeringService
+from src.spira_training.shared.core.services.feature_engineering_service import FeatureEngineeringService, \
+    AudioRepository
 from src.spira_training.shared.core.services.randomizer import Randomizer
-from src.spira_training.shared.ports.config_loader import ConfigLoader
-from src.spira_training.shared.ports.dataset_repository import DatasetRepository
+from tests.fakes.fake_dataset_repository import FakeDatasetRepository
+
+
 def main():
     config = JsonConfigLoader().load_feature_engineering_config("path/to/config")
     randomizer = Randomizer.initialize_random()
-    dataset_repository = DatasetRepository()
-    service = FeatureEngineeringService(config, randomizer, dataset_repository)
+    dataset_repository = FakeDatasetRepository()
+    csv_valid_path_reader = CSVValidPathReader()
+    audio_repository = AudioRepository()
+
+    service = FeatureEngineeringService(config, randomizer, dataset_repository, csv_valid_path_reader, audio_repository)
 
     service.execute()
 
