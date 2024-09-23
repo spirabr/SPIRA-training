bf5b853d171f6869116965101f706075b77e7b6e
Lucas Quaresma <lucasqml08@usp.br>
2024-09-23 20:39:22 -0300

    fix: imports in feature_engineering_config.py

--- src/spira_training/apps/feature_engineering/configs/feature_engineering_config.py
+++ src/spira_training/apps/feature_engineering/configs/feature_engineering_config.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel
 
-from audio_config import AudioConfig
-from audio_processor_config import AudioProcessorConfig
-from audio_feature_transformer_config import AudioFeatureTransformerConfig
+from src.spira_training.apps.feature_engineering.configs.audio_config import AudioConfig
+from src.spira_training.apps.feature_engineering.configs.audio_processor_config import AudioProcessorConfig
+from src.spira_training.apps.feature_engineering.configs.audio_feature_transformer_config import AudioFeatureTransformerConfig
 
 class FeatureEngineeringConfig(BaseModel):
     audio: AudioConfig
