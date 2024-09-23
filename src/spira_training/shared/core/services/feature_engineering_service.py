112bdaefbb822b18f594d9d7ef0b46cfbd735faa
Lucas Quaresma <lucasqml08@usp.br>
2024-09-23 20:40:24 -0300

    feat: use PathValidator and FileReader in feature_engineering_service.py

--- src/spira_training/shared/core/services/feature_engineering_service.py
+++ src/spira_training/shared/core/services/feature_engineering_service.py
@@ -1,8 +1,12 @@
+from pathlib import Path
+
 from src.spira_training.apps.feature_engineering.configs.feature_engineering_config import FeatureEngineeringConfig
 from src.spira_training.shared.core.interfaces.random import Random
 from src.spira_training.shared.ports import dataset_repository
 from src.spira_training.shared.ports.audios_repository import AudiosRepository
 from src.spira_training.shared.ports.dataset_repository import DatasetRepository
+from src.spira_training.shared.ports.file_reader import FileReader
+from src.spira_training.shared.ports.path_validator import PathValidator
 from src.spira_training.shared.ports.valid_path_reader import ValidPathReader
 
 class FeatureEngineeringService:
@@ -12,13 +16,17 @@ class FeatureEngineeringService:
             randomizer: Random,
             dataset_repository: DatasetRepository,
             valid_path_reader: ValidPathReader,
-            audios_repository: AudiosRepository
+            audios_repository: AudiosRepository,
+            file_reader: FileReader,
+            path_validator: PathValidator
     ):
         self.config = config
         self.randomizer = randomizer
         self.dataset_repository = dataset_repository
         self.valid_path_reader = valid_path_reader
         self.audios_repository = audios_repository
+        self.file_reader = file_reader
+        self.path_validator = path_validator
 
     def execute(self) -> None:
         patients_inputs, controls_inputs, noises = self._load_data()
@@ -32,9 +40,12 @@ class FeatureEngineeringService:
 
         return patients_inputs, controls_inputs, noises
 
-    def _load_audio_data(self, csv_path):
-        valid_paths = ValidPathReader.read_valid_paths(csv_path)
-        audio_data = self._load_audio_from_paths(valid_paths)
+    def _load_audio_data(self, csv_path: Path):
+        validated_csv_path = self.path_validator.validate_path(csv_path)
+        file_paths = self.file_reader.read(str(validated_csv_path))
+        validated_file_paths = [self.path_validator.validate_path(Path(path)) for path in file_paths]
+        audio_data = self._load_audio_from_paths(validated_file_paths)
+
         return audio_data
 
     def _load_audio_from_paths(self, paths):
