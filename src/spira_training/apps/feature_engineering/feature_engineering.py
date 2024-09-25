8dff58d2425be791e2d39cbaec9bbde80d1fdc3d
Lucas Quaresma <lucasqml08@usp.br>
2024-09-25 18:41:36 -0300

    refactor: FeatureEngineering dependencies for FeatureEngineeringService

--- src/spira_training/apps/feature_engineering/feature_engineering.py
+++ src/spira_training/apps/feature_engineering/feature_engineering.py
@@ -1,21 +1,37 @@
-from src.spira_training.shared.adapters.CSVValidPathReader import CSVValidPathReader
+import asyncio
+from pathlib import Path
+
 from src.spira_training.shared.adapters.JsonConfigLoader import JsonConfigLoader
-from src.spira_training.shared.core.services.feature_engineering_service import FeatureEngineeringService, \
-    AudiosRepository
+from src.spira_training.shared.core.services.feature_engineering_service import FeatureEngineeringService
 from src.spira_training.shared.core.services.randomizer import Randomizer
 from tests.fakes.fake_dataset_repository import FakeDatasetRepository
+from tests.fakes.fake_audios_repository import FakeAudiosRepository
+from tests.fakes.fake_path_validator import FakePathValidator
+from tests.fakes.fake_file_reader import FakeFileReader
 
-
-def main():
+async def main():
     config = JsonConfigLoader().load_feature_engineering_config("path/to/config")
+
+    # TODO - Instantiate the real dependencies
     randomizer = Randomizer.initialize_random()
     dataset_repository = FakeDatasetRepository()
-    csv_valid_path_reader = CSVValidPathReader()
-    audios_repository = AudiosRepository()
+    audios_repository = FakeAudiosRepository()
+    file_reader = FakeFileReader()
+    path_validator = FakePathValidator()
 
-    service = FeatureEngineeringService(config, randomizer, dataset_repository, csv_valid_path_reader, audios_repository)
+    service = FeatureEngineeringService(
+        config=config,
+        randomizer=randomizer,
+        dataset_repository=dataset_repository,
+        audios_repository=audios_repository,
+        file_reader=file_reader,
+        path_validator=path_validator,
+    )
 
-    service.execute()
+    # TODO - Get the bucket name to save the dataset
+    save_dataset_path = Path("any_model_storage_path")
+
+    await service.execute(save_dataset_path=save_dataset_path)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
