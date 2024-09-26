dc60bc51de0c88b26c63adf977fee19b1f53fd3c
Lucas Quaresma <lucasqml08@usp.br>
2024-09-25 22:17:37 -0300

    refactor: Improve FakePathValidator.validate_path implementation

--- tests/fakes/fake_path_validator.py
+++ tests/fakes/fake_path_validator.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+
+from src.spira_training.shared.core.models.valid_path import ValidPath
 from src.spira_training.shared.ports.path_validator import PathValidator
 
 class FakePathValidator(PathValidator):
-    def validate_path(self, path: str) -> str:
-        return path
+    def validate_path(self, path: Path) -> ValidPath:
+        return ValidPath(path=path)
