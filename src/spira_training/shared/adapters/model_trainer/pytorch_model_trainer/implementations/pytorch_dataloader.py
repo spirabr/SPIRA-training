3b5afb1c47c9ac04e50e8ab50039462f495d2642
Roberto Bolgheroni <robertobolgheroni@usp.br>
2024-10-03 21:17:11 -0300

    feat: support test/train dataloader factories

--- src/spira_training/shared/adapters/model_trainer/pytorch_model_trainer/implementations/pytorch_dataloader.py
+++ src/spira_training/shared/adapters/model_trainer/pytorch_model_trainer/implementations/pytorch_dataloader.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Literal, Sequence
 from .pytorch_dataset import PytorchDataset
 from src.spira_training.shared.adapters.model_trainer.pytorch_model_trainer.interfaces.dataloader import (
     Dataloader,
@@ -7,19 +7,36 @@ from src.spira_training.shared.core.models.dataset import Dataset
 from torch.utils.data import DataLoader as PytorchDataLoader
 
 
+PytorchDataloaderType = Literal["train", "test"]
+
+
 class PytorchDataloader(Dataloader):
     def __init__(
-        self, dataset: PytorchDataset, batch_size: int, num_workers: int
+        self,
+        dataset: PytorchDataset,
+        batch_size: int,
+        num_workers: int,
+        dataloader_type: PytorchDataloaderType,
     ) -> None:
-        self._py_dataloader = PytorchDataLoader(
-            dataset=dataset,
-            batch_size=batch_size,
-            num_workers=num_workers,
-            shuffle=True,
-            pin_memory=True,
-            drop_last=True,
-            sampler=None,
-        )
+        if dataloader_type == "train":
+            self._py_dataloader = PytorchDataLoader(
+                dataset=dataset,
+                batch_size=batch_size,
+                num_workers=num_workers,
+                shuffle=True,
+                pin_memory=True,
+                drop_last=True,
+                sampler=None,
+            )
+        else:
+            self._py_dataloader = PytorchDataLoader(
+                dataset=dataset,
+                batch_size=batch_size,
+                num_workers=num_workers,
+                shuffle=False,
+            )
+
+        self._dataloader_type = dataloader_type
 
     def get_batches(self) -> Sequence[Dataset]:
         output: Sequence[Dataset] = []
--- src/spira_training/shared/adapters/model_trainer/pytorch_model_trainer/implementations/pytorch_dataloader_factory.py
+++ src/spira_training/shared/adapters/model_trainer/pytorch_model_trainer/implementations/pytorch_dataloader_factory.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from .pytorch_dataset import PytorchDataset
 from .pytorch_dataloader import PytorchDataloader
 from src.spira_training.shared.adapters.model_trainer.pytorch_model_trainer.interfaces.dataloader import (
@@ -8,11 +9,19 @@ from src.spira_training.shared.adapters.model_trainer.pytorch_model_trainer.inte
     DataloaderFactory,
 )
 
+PytorchDataloaderFactoryType = Literal["train", "test"]
+
 
 class PytorchDataloaderFactory(DataloaderFactory):
-    def __init__(self, batch_size: int, num_workers: int) -> None:
+    def __init__(
+        self,
+        batch_size: int,
+        num_workers: int,
+        dataloader_type: PytorchDataloaderFactoryType,
+    ) -> None:
         self._batch_size = batch_size
         self._num_workers = num_workers
+        self._dataloader_type: PytorchDataloaderFactoryType = dataloader_type
 
     def make_dataloader(self, dataset: Dataset) -> Dataloader:
         pytorch_dataset = PytorchDataset(
@@ -22,4 +31,5 @@ class PytorchDataloaderFactory(DataloaderFactory):
             dataset=pytorch_dataset,
             batch_size=self._batch_size,
             num_workers=self._num_workers,
+            dataloader_type=self._dataloader_type,
         )
