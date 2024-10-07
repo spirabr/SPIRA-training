57623fe9184b2607dd2477f176944e4a33808ae6
Lucas Quaresma <lucasqml08@usp.br>
2024-10-07 12:09:53 -0300

    refactor: move AudioProcessor to core

--- src/spira_training/shared/core/audio_processor_factory.py
+++ src/spira_training/shared/core/audio_processor_factory.py
@@ -1,9 +1,9 @@
 from src.spira_training.apps.feature_engineering.configs.audio_processor_config import AudioProcessorType, \
     AudioProcessorConfig
-from src.spira_training.shared.adapters.audio_processor.melspectrogram_feature_transformer import MelspectrogramFeatureTransformer
-from src.spira_training.shared.adapters.audio_processor.mfcc_feature_transformer import MFCCFeatureTransformer
-from src.spira_training.shared.adapters.audio_processor.spectrogram_feature_transformer import SpectrogramFeatureTransformer
-from src.spira_training.shared.ports.audio_processor import AudioProcessor
+from src.spira_training.shared.adapters.feature_transformer.melspectrogram_feature_transformer import MelspectrogramFeatureTransformer
+from src.spira_training.shared.adapters.feature_transformer.mfcc_feature_transformer import MFCCFeatureTransformer
+from src.spira_training.shared.adapters.feature_transformer.spectrogram_feature_transformer import SpectrogramFeatureTransformer
+from src.spira_training.shared.core.audio_processor import AudioProcessor
 from src.spira_training.shared.ports.feature_transformer import FeatureTransformer
 
 
