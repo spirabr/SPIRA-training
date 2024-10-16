06ca88398c3744d73221123ef7ab49609212a724
Roberto Bolgheroni <robertobolgheroni@usp.br>
2024-10-16 19:51:35 -0300

    fix: feature engineering imports

--- src/spira_training/shared/core/services/feature_engineering_service.py
+++ src/spira_training/shared/core/services/feature_engineering_service.py
@@ -1,9 +1,15 @@
 from pathlib import Path
 
-from src.spira_training.apps.feature_engineering.configs.feature_engineering_config import FeatureEngineeringConfig
-from src.spira_training.shared.adapters.model_trainer.pytorch_model_trainer.interfaces.pytorch_audio_factory import \
-    PytorchAudioFactory
-from src.spira_training.shared.core.audio_processor_factory import create_audio_processor
+from src.spira_training.shared.adapters.pytorch.model_trainer.interfaces.pytorch_audio_factory import (
+    PytorchAudioFactory,
+)
+
+from src.spira_training.apps.feature_engineering.configs.feature_engineering_config import (
+    FeatureEngineeringConfig,
+)
+from src.spira_training.shared.core.audio_processor_factory import (
+    create_audio_processor,
+)
 from src.spira_training.shared.core.interfaces.random import Random
 from src.spira_training.shared.core.models.valid_path import ValidPath
 from src.spira_training.shared.ports.audios_repository import AudiosRepository
@@ -11,16 +17,17 @@ from src.spira_training.shared.ports.dataset_repository import DatasetRepository
 from src.spira_training.shared.ports.file_reader import FileReader
 from src.spira_training.shared.ports.path_validator import PathValidator
 
+
 class FeatureEngineeringService:
     def __init__(
-            self,
-            config: FeatureEngineeringConfig,
-            randomizer: Random,
-            dataset_repository: DatasetRepository,
-            audios_repository: AudiosRepository,
-            file_reader: FileReader,
-            path_validator: PathValidator,
-            pytorch_audio_factory: PytorchAudioFactory
+        self,
+        config: FeatureEngineeringConfig,
+        randomizer: Random,
+        dataset_repository: DatasetRepository,
+        audios_repository: AudiosRepository,
+        file_reader: FileReader,
+        path_validator: PathValidator,
+        pytorch_audio_factory: PytorchAudioFactory,
     ):
         self.pytorch_audio_factory = pytorch_audio_factory
         self.config = config
@@ -33,15 +40,21 @@ class FeatureEngineeringService:
     async def execute(self, save_dataset_path: Path) -> None:
         patients_inputs, controls_inputs, noises = self._load_data()
 
-        audio_processor = create_audio_processor(self.config.audio_processor, self.pytorch_audio_factory)
+        audio_processor = create_audio_processor(
+            self.config.audio_processor, self.pytorch_audio_factory
+        )
 
         dataset = self._generate_dataset()
 
         await self.dataset_repository.save_dataset(dataset, save_dataset_path)
 
     def _load_data(self):
-        patients_inputs = self._load_audio_data(self.config.audio.dataset_paths.patients_csv)
-        controls_inputs = self._load_audio_data(self.config.audio.dataset_paths.controls_csv)
+        patients_inputs = self._load_audio_data(
+            self.config.audio.dataset_paths.patients_csv
+        )
+        controls_inputs = self._load_audio_data(
+            self.config.audio.dataset_paths.controls_csv
+        )
         noises = self._load_audio_data(self.config.audio.dataset_paths.noises_csv)
 
         return patients_inputs, controls_inputs, noises
@@ -49,8 +62,12 @@ class FeatureEngineeringService:
     def _load_audio_data(self, csv_path: Path):
         validated_csv_path = self.path_validator.validate_path(csv_path)
         file_paths = self.file_reader.read(str(validated_csv_path))
-        validated_file_paths = [self.path_validator.validate_path(Path(path)) for path in file_paths]
-        audio_data = [self._load_audio_from_paths(path) for path in validated_file_paths]
+        validated_file_paths = [
+            self.path_validator.validate_path(Path(path)) for path in file_paths
+        ]
+        audio_data = [
+            self._load_audio_from_paths(path) for path in validated_file_paths
+        ]
 
         return audio_data
 
@@ -59,4 +76,4 @@ class FeatureEngineeringService:
 
     def _generate_dataset(self):
         pass
-        #TODO - generate dataset
+        # TODO - generate dataset
