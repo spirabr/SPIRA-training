6ab2e2b292a376b009117d6542d3e554009c6d13
Lucas Quaresma <lucasqml08@usp.br>
2024-10-23 15:02:58 -0300

    feat: create useful methods for audios classes

--- src/spira_training/shared/core/models/audio_collection.py
+++ src/spira_training/shared/core/models/audio_collection.py
@@ -1,4 +1,4 @@
-from typing import Iterator
+from typing import Iterator, Optional
 
 from src.spira_training.shared.core.models.audio import Audio
 
@@ -7,6 +7,33 @@ class AudioCollection:
     def __init__(self, audios: list[Audio], hop_length: int):
         self.audios = audios
         self.hop_length = hop_length
+        self._min_audio_length: Optional[int] = None
+        self._max_audio_length: Optional[int] = None
 
     def __iter__(self) -> Iterator[Audio]:
         return iter(self.audios)
+
+    def __len__(self) -> int:
+        return len(self.audios)
+
+
+    def copy_using(self, audios: list[Audio]) -> 'AudioCollection':
+        return AudioCollection(audios, self.hop_length)
+
+    def get_max_audio_length(self) -> int:
+        if self._max_audio_length is None:
+            self._calculate_min_max_audio_length()
+        return self._max_audio_length
+
+    def get_min_audio_length(self) -> int:
+        if self._min_audio_length is None:
+            self._calculate_min_max_audio_length()
+        return self._min_audio_length
+
+    def _calculate_min_max_audio_length(self):
+        audio_lengths = [self._calculate_audio_length(audio) for audio in self.audios]
+        self._min_audio_length = min(audio_lengths)
+        self._max_audio_length = max(audio_lengths)
+
+    def _calculate_audio_length(self, audio: Audio) -> int:
+        return int((audio.wav.shape[1] / self.hop_length) + 1)
--- src/spira_training/shared/core/models/generated_audio_collection.py
+++ src/spira_training/shared/core/models/generated_audio_collection.py
@@ -1,6 +1,12 @@
-from src.spira_training.shared.core.models.generated_audio import GeneratedAudio
+from src.spira_training.shared.core.models.audio import Audio
 
 
 class GeneratedAudioCollection:
-    def __init__(self, generated_audios: list[GeneratedAudio]):
+    def __init__(self, generated_audios: list['Audio']):
         self.generated_audios = generated_audios
+
+    def __len__(self) -> int:
+        return len(self.generated_audios)
+
+    def copy_using(self, audios: list['Audio']) -> 'GeneratedAudioCollection':
+        return GeneratedAudioCollection(audios)
