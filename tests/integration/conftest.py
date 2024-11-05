7b237e96ca129d6866c69af1a20023917078d598
Roberto Bolgheroni <robertobolgheroni@usp.br>
2024-11-04 21:14:24 -0300

    feat: add train loss calculator smoke test

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,9 +1,22 @@
 import pytest
+from spira_training.shared.adapters.pytorch.model_trainer.implementations.pytorch_loss_calculator.multiple_loss_calculator import (
+    AverageMultipleLossCalculator,
+)
+from src.spira_training.shared.adapters.pytorch.model_trainer.implementations.simple_pytorch_dataloader_factory import (
+    SimplePytorchDataloaderFactory,
+)
+from src.spira_training.shared.adapters.pytorch.model_trainer.interfaces.pytorch_loss_calculator import (
+    PytorchLossCalculator,
+)
 import torch
 from src.spira_training.shared.adapters.pytorch.model_trainer.implementations.simple_pytorch_optimizer import (
     SimplePytorchOptimizer,
 )
 from torch.optim import Adam
+from src.spira_training.shared.adapters.pytorch.model_trainer.implementations.pytorch_loss_calculator.single_loss_calculator import (
+    BCELossCalculator,
+    SingleLossCalculator,
+)
 
 
 @pytest.fixture()
@@ -22,3 +35,39 @@ def inner_torch_optimizer():
 @pytest.fixture()
 def pytorch_optimizer(inner_torch_optimizer):
     return SimplePytorchOptimizer(torch_optimizer=inner_torch_optimizer)
+
+
+@pytest.fixture()
+def train_dataloader_factory() -> SimplePytorchDataloaderFactory:
+    return SimplePytorchDataloaderFactory(
+        batch_size=1,
+        dataloader_type="train",
+        num_workers=1,
+        pytorch_tensor_factory=None,
+    )
+
+
+@pytest.fixture()
+def test_dataloader_factory() -> SimplePytorchDataloaderFactory:
+    return SimplePytorchDataloaderFactory(
+        batch_size=1,
+        dataloader_type="test",
+        num_workers=1,
+        pytorch_tensor_factory=None,
+    )
+
+
+@pytest.fixture()
+def single_train_loss_calculator() -> SingleLossCalculator:
+    return BCELossCalculator(
+        reduction="none",
+    )
+
+
+@pytest.fixture()
+def train_loss_calculator(
+    single_train_loss_calculator: SingleLossCalculator,
+) -> PytorchLossCalculator:
+    return AverageMultipleLossCalculator(
+        single_loss_calculator=single_train_loss_calculator,
+    )
